8c8b332ccc25b7a156797e5350fc688537b93e72
Jaiveer <jaiveersuthar99@gmail.com>
2021-06-30 18:42:39 +0530

    first

---
+++ build/static/js/main.82a1c60d.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpgallery=this.webpackJsonpgallery||[]).push([[0],{11:function(n,e,t){"use strict";t.r(e);t(1);var c=t(3),o=t.n(c),i=(t(8),t.p,t(9),t(0));var r=function(n){n&&n instanceof Function&&t.e(3).then(t.bind(null,12)).then((function(e){var t=e.getCLS,c=e.getFID,o=e.getFCP,i=e.getLCP,r=e.getTTFB;t(n),c(n),o(n),i(n),r(n)}))},l=Object(i.jsx)("h1",{children:"hello"});o.a.render(l,document.getElementById("root")),r()},8:function(n,e,t){},9:function(n,e,t){}},[[11,1,2]]]);
+//# sourceMappingURL=main.82a1c60d.chunk.js.map
